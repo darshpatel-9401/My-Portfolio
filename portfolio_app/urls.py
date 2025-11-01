cf8d9730f767c2c3a5c1c16bde67f69e2a47753b
DARSH PATEL <darshpatel9401@gmail.com>
2025-11-01 18:05:04 +0530

    Update urls.py

--- portfolio_app/urls.py
+++ portfolio_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.home, name='index'),
     path('resume/', views.resume, name='resume'),
     path('projects/', views.projects, name='projects'),
     path('contact/', views.contact, name='contact'),
