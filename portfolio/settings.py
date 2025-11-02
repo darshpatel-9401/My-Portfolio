6cac9da0bb43d8548578a4cb957fcea47329b52a
DARSH PATEL <darshpatel9401@gmail.com>
2025-11-02 12:42:27 +0530

    Update settings.py

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -108,7 +108,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / 'portfolio_app' / 'static',]
+STATICFILES_DIRS = [BASE_DIR / 'portfolio_app' / 'static']
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
